788e63f7fd73f407279638547890b910d3547444
ehab taleb <etaleb.ext@galerieslafayette.com>
2014-07-29 17:25:48 +0200

    Max array in O(n)

--- src/main/java/algo/MaxSubArray.java
+++ src/main/java/algo/MaxSubArray.java
@@ -22,28 +22,31 @@ public class MaxSubArray {
 		init(origin);
 		print(origin);
 		int max = 0;
-		for(int i = 0; i < origin.length; i++){
-			//int sum = sumSubArray(i, origin);
-			int sum = 0;
-			for(int j = 0; j <= i; j++){
-					sum += origin[j];
-			}
-			System.out.printf("for i %d the sum is %d \n", i, sum);
-			if(sum > max)
-				max = sum;
+        int[] sumArr = new int[origin.length];
+        sumArr[0] = origin[0];
+
+        for (int i = 1; i < origin.length; i++) {
+            sumArr[i] = sumArr[i - 1] + origin[i];
+
+            // for(int j = 0; j <= i; j++){
+            // sum += origin[j];
+            // }
+            System.out.printf("for i %d the sum is %d \n", i, sumArr[i]);
+            if (sumArr[i] > max)
+                max = sumArr[i];
 		}
-		
 		System.out.println(max);
 
 	}
-	
-	private static int sumSubArray(int idx, int[] origin) {
-		int sum = 0;
-		for(int i = 0; i <= idx; i++){
-				sum += origin[i];
-		}
-		return sum;
-	}
+
+    //
+    // private static int sumSubArray(int idx, int[] origin) {
+    // int sum = 0;
+    // for(int i = 0; i <= idx; i++){
+    // sum += origin[i];
+    // }
+    // return sum;
+    // }
 
 	private static void print(int[] origin2) {
 		for(int i = 0; i < origin.length; i++){
